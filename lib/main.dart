978e25c946667c40fb4fe12705ea400250114327
Jake <jakewarrenblack01@gmail.com>
2021-08-22 16:45:48 +0100

    Update theme, move input widget to input_page.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,21 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
 
 void main() => runApp(BMICalculator());
 
+// the main.dart file should contain most of the theming, then direct towards the first screen
+// for every subsequent screen, we'll have a separate dart file
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      // .copyWith() allows us to use a default theme while overriding certain aspects
+      // you can also wrap individual widgets with a Theme widget and provide the 'data' property to change them specifically
+      theme: ThemeData.dark().copyWith(
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(0xFF0A0E21),
+      ),
       home: InputPage(),
-    );
-  }
-}
-
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-      ),
     );
   }
 }
